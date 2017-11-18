25db302143680cbdf51c7ee96bd2ff843b30cafb
paztis <jerome.henaff@gmail.com>
2017-11-18 17:08:45 +0100

    update promise management

--- index.js
+++ index.js
@@ -15,58 +15,70 @@ module.exports = function (content) {
         .replace(/\u2029/g, '\\u2029');
 
     var result =
+        'var returnedPromise;' +
         'var scriptId="' + scriptId + '";' +
-        'var promise = {'+
-            'status: 0,' +
-            'successCb: [],' +
-            'failureCb: [],' +
-            '_dispatch: function(cbList, arg) {' +
-                'var cb;' +
-                'while(cbList.length) {' +
-                    'cb = cbList.shift();' +
-                    'cb && cb(arg);' +
+        'var script = document.getElementById(scriptId);' +
+        'if(!script) {' +
+            // Create a promise-like structure in case of old browsers
+            'returnedPromise = {'+
+                'status: 0,' +
+                'arg: null,' +
+                'successCb: [],' +
+                'failureCb: [],' +
+                '_dispatch: function(cbList, arg) {' +
+                    'var cb;' +
+                    'while(cbList.length) {' +
+                        'cb = cbList.shift();' +
+                        'cb && cb(arg);' +
+                    '}' +
+                '},' +
+                'resolve: function(arg) {' +
+                    'this.status = 1;' +
+                    'this.arg = arg;' +
+                    'this._dispatch(this.successCb, arg);' +
+                '},' +
+                'reject: function(err) {' +
+                    'this.status = 2;' +
+                    'this.arg = err;' +
+                    'this._dispatch(this.failureCb, err);' +
+                '},' +
+                '_listen: function(cbList, cb, status) {' +
+                    'cbList.push(cb);' +
+                    'if(this.status === status) {this._dispatch(cbList, this.arg);}' +
+                '},' +
+                'then: function(cb) {' +
+                    'this._listen(this.successCb, cb, 1);' +
+                '},' +
+                'catch: function(cb) {' +
+                    'this._listen(this.failureCb, cb, 2);' +
                 '}' +
-            '},' +
-            'resolve: function(arg) {' +
-                'this.status = 1;' +
-                'this._dispatch(this.successCb, arg);' +
-            '},' +
-            'reject: function(err) {' +
-                'this.status = 2;' +
-                'this._dispatch(this.failureCb, err);' +
-            '},' +
-            '_listen: function(cbList, cb, status) {' +
-                'cbList.push(cb);' +
-                'if(this.status === status) {' +
-                    'this._dispatch(cbList, arg);' +
-                '}' +
-            '},' +
-            'then: function(cb) {' +
-                'this._listen(this.successCb, cb);' +
-            '},' +
-            'catch: function(cb) {' +
-                'this._listen(this.failureCb, cb);' +
-            '}' +
-        '};' +
-        'var loaded = function() {};' +
-        'if(!document.getElementById(scriptId)) {' +
+            '};' +
+
+            // Blob url management
             'var blob = new Blob([' + codeString + ']);' +
-            'var script = document.createElement("script");' +
             'var url = URL.createObjectURL(blob);' +
             'var revoqueUrl = function() {URL.revokeObjectURL(url);};' +
-            'script.onerror = revoqueUrl;' +
+
+            'script = document.createElement("script");' +
+            // On script error loading
+            'script.onerror = function(){' +
+                'revoqueUrl();' +
+                'returnedPromise.reject()' +
+            '};' +
+            // On script success loading
             'script.onload = function(){' +
                 'revoqueUrl();' +
-                'promise.resolve()' +
+                'returnedPromise.resolve()' +
             '};' +
+            'script.loadingPromise = returnedPromise;' + // Store the promise in the scipt
             'script.id = scriptId;' +
-            'script.async = true;' +
+            'script.async = true;' + // Set the script async
             'script.src = url;' +
             'document.body.appendChild(script);' +
         '} else {' +
-            'promise.resolve()' +
+            'returnedPromise = script.loadingPromise;' +
         '}' +
-        'module.exports=promise;';
+        'module.exports=returnedPromise;';
 
     return result;
 };
